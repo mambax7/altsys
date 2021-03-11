8bf8347b21cc57ca1997e14b0c9cfd215a525919
mambax7 <mambax7@gmail.com>
2021-03-11 06:08:12 -0500

    static called as dynamic

--- xoops_trust_path/libs/altsys/oninstall.php
+++ xoops_trust_path/libs/altsys/oninstall.php
@@ -60,12 +60,12 @@ if (!function_exists('altsys_oninstall_base')) {
 
             $sql_query = trim(file_get_contents($sql_file_path));
 
-            $sqlutil->splitMySqlFile($pieces, $sql_query);
+            $sqlutil::splitMySqlFile($pieces, $sql_query);
 
             $created_tables = [];
 
             foreach ($pieces as $piece) {
-                $prefixed_query = $sqlutil->prefixQuery($piece, $prefix_mod);
+                $prefixed_query = $sqlutil::prefixQuery($piece, $prefix_mod);
 
                 if (!$prefixed_query) {
                     $ret[] = 'Invalid SQL <b>' . htmlspecialchars($piece, ENT_QUOTES | ENT_HTML5) . '</b><br />';
